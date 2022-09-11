fa85bff6e255a0e93a175eda15daec21c876f224
rattadduhee <shinjh1208@naver.com>
2022-09-11 18:57:31 +0900

    0910

---
+++ 나동빈/구현/4-1.py
@@ -0,0 +1,19 @@
+import sys
+
+num = int(input())
+s = list(sys.stdin.readline().strip().split(' '))
+x = 1
+y = 1
+print(s)
+
+for i in s:
+    if (i == 'L') and (y > 1):
+        y -= 1
+    if (i == 'R') and (y < num):
+        y += 1
+    if (i == 'U') and (x > 1):
+        x -= 1
+    if (i == 'D') and (x < num):
+        x += 1
+
+print(x, y)
